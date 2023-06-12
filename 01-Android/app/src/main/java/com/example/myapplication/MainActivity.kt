f6bbeb69a084316550e3a7f8b18dc2474b53c76e
Dennis Lincango <dennis.lincango@epn.edu.ec>
2023-06-12 12:14:30 -0500

    Update

--- 01-Android/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida.setOnClickListener {
+            irActividad(ACicloVida::class.java)
+        }
     }
+
+    fun irActividad(
+        clase: Class<*>
+    ) {
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
+
 }
